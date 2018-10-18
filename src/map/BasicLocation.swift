d47bdf95606d3cfe9297754d87ee2456a1ed4c8d
DevFatani <I_fatani@outlook.com>
2018-10-18 09:55:59 +0300

    add new methods

--- src/map/BasicLocation.swift
+++ src/map/BasicLocation.swift
@@ -2,7 +2,7 @@
  
  import CoreLocation
  
- extension UR_CLASS: CLLocationManagerDelegate {
+ class UR_CLASS: CLLocationManagerDelegate {
 
     
     fileprivate var isSetLocation = false
@@ -22,11 +22,10 @@
         self.locationManager.startMonitoringSignificantLocationChanges()
     }
 
-     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    // Impl Methods
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error while get location \(error)")
     }
-    
-
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if !self.isSetLocation {
@@ -34,6 +33,28 @@
             self.userLocation = locations.last
         }
         self.locationManager.stopUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            // If status has not yet been determied, ask for authorization
+            manager.requestWhenInUseAuthorization()
+            break
+        case .authorizedWhenInUse, .authorizedAlways:
+            // If authorized when in use
+            manager.startUpdatingLocation()
+            break
+        case .restricted, .denied:
+            if let url = NSURL(string: UIApplicationOpenSettingsURLString) as URL? {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                } else {
+                    // Fallback on earlier versions
+                }
+            }
+            break
+        }
     }
     
 }
